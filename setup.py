53e8082124de1dad7e1f9cea98a15f8254f82ff7
bornwitbugs <40208858+bornwitbugs@users.noreply.github.com>
2019-01-31 06:29:31 +0000

    final packaging fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-from os import path
 from setuptools import setup
 
 __version__ = '0.2.8'
 
 
 def read(*paths):
-    with open(path.join(*paths), 'r') as f:
+    with open(*paths, 'r') as f:
         return f.read()
 
 
@@ -15,12 +14,12 @@ setup(
     name='ftpretty',
     version=__version__,
 
-    packages=['ftpretty',],
+    packages=['ftpretty', ],
 
     author='bornwitbugs',
     author_email='40208858+bornwitbugs@users.noreply.github.com',
     description='Pretty FTP Wrapper',
-    download_url='https://github.com/bornwitbugs/ftpretty/tarball/{0}'.format(__version__),
+    download_url='https://pypi.python.org/pypi/ftpretty',
     license='BSD',
     long_description=(
         read('README.rst') + '\n\n' +
@@ -30,7 +29,7 @@ setup(
     keywords='ftp ftps file transfer protocal',
     platforms=['any'],
     test_suite = 'tests.test_ftpretty.suite',
-    url='https://github.com/bornwitbugs/ftpretty/',
+    url='https://github.com/bornwitbugs/ftpretty',
     classifiers=[
         "Topic :: Internet :: File Transfer Protocol (FTP)",
         "Topic :: Utilities",
